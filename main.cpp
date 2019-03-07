2c48da018ef90517ade11f95d22187a6dfc6c288
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-03-06 23:25:06 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author: Christa Hatch
+ * @version: 1.0
+ * @Course: CS 151 - Advanced C++ Programming
+ * @Date: 03/06/19
+ *
+ * Problem Statement: Movie Statistics - Write a program that can be used to gather statistical
+ * data about the number of movies college students see in a month. The program should ask the user
+ * how many students were surveyed and dynamically allocate an array of that size. The program should
+ * then calculate the average, median, and mode of the values entered.
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
